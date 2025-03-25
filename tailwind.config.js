a635e1e5334977f5915d76b5d820b2c99c3fc443
DavidUmiri <davidumiri@gmail.com>
2025-03-25 19:10:07 +0100

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    // Nos aseguramos que Tailwind se aplique en todos los archivos dentro de src
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
